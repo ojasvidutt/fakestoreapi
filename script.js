fe8a930178e50487957f936af388f60904eb1779
ojasvidutt <ojasvidutt@gmail.com>
2025-04-01 23:35:59 +0530

    api integrated

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-for(i=0;i<=19;i++){
- fetch(`https://fakestoreapi.com/products/${i+1}`)
-            .then(res=>res.json())
-            .then(j=>{document.querySelector(".row").innerHTML+=`<div class="col-3">${j.title}
-            <img src="${j.image}" alt="">
-            </div>`;}
-        );
+for (i = 0; i <= 19; i++) {
+    fetch(`https://fakestoreapi.com/products/${i + 1}`)
+        .then(res => res.json())
+        .then(j => {
+            
+            let truncatedTitle = truncateText(j.title); 
+            let heading = extractTitle(j.title);
+            
+            document.querySelector(".row").innerHTML += `
+                <div class="col-3 item">
+                    <div class="image"><img src="${j.image}" alt=""></div>
+                      <h2>${heading}</h2>
+                      <p title="${j.title}">  ${truncatedTitle}</p>
+                </div>`
+
+                //made flash cards
+               
+                // document.querySelector(".flash").innerHTML +=  
+                
+            
+
+            })
+        
+
+}
+
+function truncateText(text) {
+    let words = text.split(" ");
+    return words.length > 5 ? words.slice(1, 5).join(" ") + "..." : text;
+
+}
+
+function extractTitle(text) {
+    let word = text.split(" ");
+    return word[0];
+    
 }
 
